e1072f6e4179cc53d6f39af0b893abc889ba5c5d
Alan Alvarez <aran.712@gmail.com>
2024-04-27 21:51:04 -0600

    suma de 2 elementos, creado

--- controllers/SitioController.php
+++ controllers/SitioController.php
@@ -2,14 +2,24 @@
 
 namespace app\controllers;
 
+// Controladores para la aplicación web
 use Yii;
 use yii\filters\AccessController;
 use yii\web\Controller;
 
+// Datos para la base de datos
+use app\models\FormularioForm;
+
 class SitioController extends Controller{
 
     public function actionInicio(){
-        return $this->render('inicio');
+        $model = new FormularioForm();
+
+        if($model->load(Yii::$app->request->post()) && $model->validate()){
+            $valorRespuesta = ("El resultado de la suma es: ". $model -> valorA + $model -> valorB);
+            return $this->render('inicio', ['mensaje' => $valorRespuesta, 'model' => $model]);
+        }
+        return $this->render('inicio', ['model' => $model]);
     }
 }
 ?>
--- views/sitio/inicio.php
+++ views/sitio/inicio.php
@@ -1,5 +1,21 @@
 <?php
 use yii\helpers\Html;
+use yii\widgets\ActiveForm;
 ?>
 
+<?php
+    if($mensaje){
+        echo HTML::tag('div', HTML::encode($mensaje), ['class' => 'alert alert-danger']);
+    }
+?>
 Hola Usuario
+
+<?php $formulario = ActiveForm::begin(); ?>
+<?= $formulario -> field($model, 'valorA') ?>
+<?= $formulario -> field($model, 'valorB') ?>
+
+<div class="form-group">
+    <?= Html::submitButton('Enviar', ['class' => 'btn btn-primary']) ?>
+</div>
+
+<?php ActiveForm::end(); ?>
